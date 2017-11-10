794eff88b84c27cba5ee395b83cd0ff1d736e7d0
Yvette Zhang <yvettez2016@gmail.com>
2017-11-10 16:19:33 -0500

    added two drum buttons for two tracks

--- App.js
+++ App.js
@@ -14,16 +14,40 @@ import {
 } from 'react-native';
 
 var Sound = require('react-native-sound');
+var songUrl1 = require('./assets/sounds/1.mp3')
+var songUrl2 = require('./assets/sounds/long.mp3')
 
-// Sound.setCategory('Playback');
+// var songUrl = require('./2.mp3')
 
-// const drumSound = new Sound('./assets/sounds/1.wav', (error) => {
-//   if (error) {
-//     console.log('failed to load the sound', error);
-//     return;
-//   }
-//   console.log('duration in secondes: ', drumSound.getDuration(), 'number of channels: ', drumSound.getNumberOfChannels());
-// })
+Sound.setCategory('Playback', true);
+
+
+
+const drumSound1 = new Sound(songUrl1, undefined, error => {
+  if (error) {
+    console.log('error loading sound', error)
+    return
+  }
+  drumSound1.play(() => {
+
+    // drumSound.stop()
+  })
+})
+
+const drumSound2 = new Sound(songUrl1, undefined, error => {
+  if (error) {
+    console.log('error loading sound', error)
+    return
+  }
+  drumSound2.play(() => {
+
+    // drumSound.stop()
+  })
+})
+
+const testArr = [];
+
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -46,15 +70,44 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-        <Button title="DRUM" onPress={() => {console.log('Hahahahaha!!!!!!!!')
-        // drumSound.play((success) => {
-        //   if (success) {
-        //     console.log('successfully finished playing')
-        //   } else {
-        //     console.log('playback failed du to audio decoding errors')
-        //   }
+        <Button title="TIME" onPress={() => {
+          console.log('!!!!!!!', new Date().getTime())
+          testArr.push(new Date().getTime())
+          // drumSound.play((success) => {
+          // if (success) {
+          //   // drumSound.stop()
+          //   // drumSound.release()
+          //   // console.log('successfully finished playing')
+          // } else {
+          //   console.log('playback failed due to audio decoding errors', drumSound)
+          // }
         // })
+       
       }}/>
+      <Button title="Calculate" onPress={() => {
+        const zero = testArr[0]
+        console.log(testArr.map((x,i) => x-testArr[i-1]))
+      }} />
+      <Button title="DRUM1" onPress={() => {
+        drumSound1.play((success) => {
+          if (success) {
+            drumSound1.stop()
+          } else {
+            console.log('playback failed due to audio decoding errors')
+          }
+        })
+      }}
+      />
+      <Button title="DRUM2" onPress={() => {
+        drumSound2.play((success) => {
+          if (success) {
+            drumSound2.stop()
+          } else {
+            console.log('playback failed due to audio decoding errors')
+          }
+        })
+      }}
+      />
       </View>
     );
   }
