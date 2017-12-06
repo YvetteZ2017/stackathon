2f6ea5779e7f91af91cae38cc91e467f88eeca19
Yvette Zhang <yvettez2016@gmail.com>
2017-12-06 18:33:44 -0500

    feat: forecast curve

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,57 +1,102 @@
 import React, { Component } from 'react';
 import { ART, AppRegistry, StyleSheet, Text, View, Button, Image } from 'react-native';
-import * as d3 from 'd3';
+import * as scale from 'd3-scale';
+import * as shape from 'd3-shape';
 const { Surface, Group, Shape } = ART;
+const d3 = { scale, shape };
 
 export default class Forecast extends Component {
-  
+
+  constructor() {
+    super();
+    this.state = {
+      curve: ''
+    }
+    this.generateCurve = this.generateCurve.bind(this)
+  }
+
+  componentDidMount() {
+    const curve = this.generateCurve();
+    this.setState({ curve })
+  }
+
+  generateCurve() {
+    const shorterList = this.props.shorterList;
+    const lastElement = shorterList.pop();
+    const width = 300;
+    
+    if(shorterList.length) {
+      const scaleX = scale.scaleLinear()
+      .domain([shorterList[0].dt, lastElement.dt])
+      .range([0, width]);
+
+      const rangeY = shorterList.reduce((all, cur) => {
+        return [Math.min(all[0], cur.main.temp), Math.max(all[1], cur.main.temp)]
+      },[400, -100]);
+
+      const domainY = [Math.round(rangeY[0]-5), Math.round(rangeY[1]+5)];
+
+      const scaleY = scale.scaleLinear()
+      .domain(domainY)
+      .range([0, width]);
+      
+        
+      const curveLine = shape.line()
+      .x(d => scaleX(d.dt))
+      .y(d => scaleY(d.main.temp))
+      .curve(shape.curveNatural);
+
+      const curve = curveLine(shorterList)
+      return curve;
+    }
+  }
+
   render() {
     
     const shorterList = this.props.shorterList;
+    const lastElement = shorterList.pop();
+    const width = 300;
+    let curve = ''
+    
+    if(shorterList.length) {
+      const scaleX = scale.scaleLinear()
+      .domain([shorterList[0].dt, lastElement.dt])
+      .range([0, width]);
 
-    const curve = d3.curveNatural()
-    .x(function(d) { return x(d.dt); })
-    .y(function(d) { return y(d.main.temp); });
+      const rangeY = shorterList.reduce((all, cur) => {
+        return [Math.min(all[0], cur.main.temp), Math.max(all[1], cur.main.temp)]
+      },[400, -100]);
 
-    const scaleX = createScaleX(
-      data[0].time,
-      lastDatum.time,
-      width
-    );
-  
-  //       return (
-  //           <View style={styles.container_secondPage}>
-  //           <Text style={styles.title}>Forecast</Text>
-  //           {
-  //             shorterList.map((element,i) => ( 
-  //               this.props.metric ? 
-  //               <Text style={styles.main_text} key={element.dt}> {element.dt_txt.slice(5, 16)}    {Math.round(element.main.temp)} °C    {element.weather[0].main}</Text>
-  //               : <Text style={styles.main_text} key={element.dt}> {element.dt_txt.slice(5, 16)}    {Math.round(element.main.temp * 9 / 5 - 459.67)} °F    {element.weather[0].main}</Text>            
-              
-  //             ))
-  //           }
-  //         </View>
-  //       )
+      const domainY = [Math.round(rangeY[0]-5), Math.round(rangeY[1]+5)];
+
+      const scaleY = scale.scaleLinear()
+      .domain(domainY)
+      .range([0, width]);
+      
+        
+      const curveLine = shape.line()
+      .x(d => scaleX(d.dt))
+      .y(d => scaleY(d.main.temp))
+      .curve(shape.curveNatural);
+
+      curve = curveLine(shorterList)
+
+    }
 
         return (
           <View style={styles.container_secondPage}>
           <Text style={styles.title}>Forecast</Text>
+          {
+            (shorterList.length) ?
           <Surface width={320} height={320}>
             <Group x={0} y={0}>
-              <Shape
-                d={shorterList}
+                <Shape
+                d={curve}
                 stroke="#000"
                 strokeWidth={1}
               />
             </Group>
-          </Surface>
-          {
-            shorterList.map((element,i) => ( 
-              this.props.metric ? 
-              <Text style={styles.main_text} key={element.dt}> {element.dt_txt.slice(5, 16)}    {Math.round(element.main.temp)} °C    {element.weather[0].main}</Text>
-              : <Text style={styles.main_text} key={element.dt}> {element.dt_txt.slice(5, 16)}    {Math.round(element.main.temp * 9 / 5 - 459.67)} °F    {element.weather[0].main}</Text>            
-            
-            ))
+          </Surface> : null
           }
         </View>
       )
